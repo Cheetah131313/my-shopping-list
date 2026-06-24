31864aa3630d8ac2cabdea53ce96037bc3c65c5d
Anastasie Žďárská <n.nastja@seznam.cz>
2026-06-24 16:42:49 +0200

    Add MongoDB Atlas support and project documentation

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const shoppingListRoutes = require('./routes/shoppingListRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dotenv = require('dotenv');
+const PORT = process.env.PORT || 5000;
 
 // Load environment variables
 dotenv.config();
@@ -41,52 +42,8 @@ module.exports = app;
 
 // Start the server only if not in test mode
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(5000, () => {
-    console.log("Server started on port 5000");
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
   });
 }
 
-
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// const morgan = require('morgan'); 
-// const shoppingListRoutes = require('./routes/shoppingListRoutes'); // Ensure this matches the filename exactly
-// const userRoutes = require('./routes/userRoutes');
-// const dotenv = require('dotenv'); //for testing
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan('dev'));
-
-// mongoose.connect('mongodb://localhost:27017/mydatabase', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch(err => {
-//   console.error('Could not connect to MongoDB', err);
-// });
-
-// app.use('/shopping-lists', shoppingListRoutes); //Shopping list routes
-// app.use('/users', userRoutes); //User routes
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('Something broke!');
-// });
-
-// // Export app for testing
-// module.exports = app;
-
-// // Start the server only if not in test environment
-// if (process.env.NODE_ENV !== 'test') {
-//   app.listen(5000, () => {
-//     console.log("Server started on port 5000");
-//   });
-// }
